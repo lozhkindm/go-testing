e62da726c2563d5e944217b0fcfbcba68b720e25
Dmitriy <lozhkindm@yandex.ru>
2022-12-11 20:54:19 +0300

    prime table tests

--- prime/main_test.go
+++ prime/main_test.go
@@ -3,19 +3,25 @@ package main
 import "testing"
 
 func TestMain_isPrime(t *testing.T) {
-	res, msg := isPrime(0)
-	if res {
-		t.Error("expected false, but got true", 0)
-	}
-	if msg != "0 is not prime, by definition." {
-		t.Error("wrong message", msg)
+	tests := []struct {
+		name     string
+		num      int
+		expected bool
+		msg      string
+	}{
+		{name: "prime", num: 7, expected: true, msg: "7 is a prime number."},
+		{name: "not prime", num: 8, expected: false, msg: "8 is not a prime number because it is divisible by 2."},
 	}
 
-	res, msg = isPrime(7)
-	if !res {
-		t.Error("expected true, but got false", 7)
-	}
-	if msg != "7 is a prime number." {
-		t.Error("wrong message", msg)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, msg := isPrime(tt.num)
+			if res != tt.expected {
+				t.Fatal("result and expected are not equal:", res, tt.expected)
+			}
+			if msg != tt.msg {
+				t.Fatal("messages are not equal:", msg, tt.msg)
+			}
+		})
 	}
 }
