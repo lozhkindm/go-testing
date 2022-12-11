00acd7ac70df47c9f2f854200b88b6e0660aa202
Dmitriy <lozhkindm@yandex.ru>
2022-12-11 22:25:35 +0300

    prime table tests continue

--- prime/main_test.go
+++ prime/main_test.go
@@ -11,6 +11,9 @@ func TestMain_isPrime(t *testing.T) {
 	}{
 		{name: "prime", num: 7, expected: true, msg: "7 is a prime number."},
 		{name: "not prime", num: 8, expected: false, msg: "8 is not a prime number because it is divisible by 2."},
+		{name: "zero", num: 0, expected: false, msg: "0 is not prime, by definition."},
+		{name: "one", num: 1, expected: false, msg: "1 is not prime, by definition."},
+		{name: "negative", num: -11, expected: false, msg: "Negative numbers are not prime, by definition."},
 	}
 
 	for _, tt := range tests {
